a40d0e44b84b1e1181feb4a2a031f92107a683b1
kendrick hardy <kendrickl.hardy@gmail.com>
2024-02-17 18:03:43 -0600

    Added Nav bar and pages

--- Clothify/src/Clothify.jsx
+++ Clothify/src/Clothify.jsx
@@ -1,7 +1,11 @@
-
+import Navbar from "./Components/Navbar/Navbar.jsx";
 
 function Clothify() {
-  
+  return(
+    <div>
+        <Navbar/>
+    </div>
+  );
 }
 
 export default Clothify
--- Clothify/src/main.jsx
+++ Clothify/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Clothify.jsx'
+import Clothify from './Clothify.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
