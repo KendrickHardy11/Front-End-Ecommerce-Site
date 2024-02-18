738bd7bfa057c848bb31a051862adf919beb77f0
kendrick hardy <kendrickl.hardy@gmail.com>
2024-02-18 14:41:41 -0600

    Added pages to the site

--- Clothify/src/Clothify.jsx
+++ Clothify/src/Clothify.jsx
@@ -1,9 +1,24 @@
 import Navbar from "./Components/Navbar/Navbar.jsx";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import NewFeatured from "./Pages/NewFeatured.jsx";
+import Mens from "./Pages/Mens.jsx";
+import Womens from "./Pages/Womens.jsx";
+import Kids from "./Pages/Kids.jsx";
+import SignIn from "./Pages/SignIn.jsx";
+import { Product } from "./Pages/Product.jsx"
 
 function Clothify() {
   return(
     <div>
-        <Navbar/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<NewFeatured/>}/>
+          <Route path='/mens' element={<Mens/>}/>
+          <Route path='/womens' element={<Womens/>}/>
+          <Route path='/kids' element={<Kids/>}/>
+          <Route path='/signin' element={<SignIn/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- Clothify/src/Components/Navbar/Navbar.jsx
+++ Clothify/src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import "./Navbar.css"
 import logo from "../../assets/textile.png"
 import logoLarge from "../../assets/textile Large.png"
 import cart_icon from "../../assets/shopping-bag.png"
+import { Link } from "react-router-dom"
 
 function Navbar(){
     return(
@@ -11,13 +12,13 @@ function Navbar(){
                 <img src = {logoLarge} alt = "Clothify Logo"/>
             </div>
             <ul className = "nav-menu">
-                <li>New & Featured</li>
-                <li>Men</li>
-                <li>Women</li>
-                <li>Kids</li>
+                <Link to="/" className="links">New & Featured</Link>
+                <Link to="/mens" className="links">Men</Link>
+                <Link to="/womens" className="links">Women</Link>
+                <Link to="/kids" className="links">Kids</Link>
             </ul>
             <div className="Login">
-                <button className="login-Button">Sign in/Sign Up</button>
+                <Link to="signin" className="login-Button">Sign in/Sign Up</Link>
                 <img src={cart_icon} alt="cart icon"></img>
                 <div className="item-Counter">0</div>
             </div>
